accb9a3c7ac2545317c19f724a854e3864e1a36e
PurpleNoon <346014945@qq.com>
2021-04-26 23:04:21 +0800

    chore: 扩展 tailwind 配置，并禁用 jit 模式（jit 模式有 bug）

--- packages/sakura/tailwind.config.js
+++ packages/sakura/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  mode: 'jit',
   purge: [
     './index.html',
     './packages/**/*.{vue,js,ts,jsx,tsx}'
   ],
   darkMode: false, // or 'media' or 'class'
+  prefix: 'sr-',
   theme: {
     fontSize: {
       'xs': '.75rem',
@@ -20,7 +20,57 @@ module.exports = {
       '6xl': '4rem',
       '7xl': '5rem',
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#409EFF',
+          100: '#53a8ff',
+          200: '#66b1ff',
+          300: '#79bbff',
+          400: '#8cc5ff',
+          500: '#a0cfff',
+          600: '#b3d8ff',
+          700: '#c6e2ff',
+          800: '#d9ecff',
+          900: '#ecf5ff',
+        },
+        success: {
+          DEFAULT: '#67C23A',
+          800: '#e1f3d8',
+          900: '#f0f9eb',
+        },
+        warning: {
+          DEFAULT: '#E6A23C',
+          800: '#faecd8',
+          900: '#fdf6ec',
+        },
+        danger: {
+          DEFAULT: '#F56C6C',
+          800: '#fde2e2',
+          900: '#fef0f0',
+        },
+        info: {
+          DEFAULT: '#909399',
+          800: '#e9e9eb',
+          900: '#f4f4f5',
+        },
+        'text': {
+          DEFAULT: '#303133',
+          regular: '#606266',
+          secondary: '#909399',
+          placeholder: '#C0C4CC',
+        },
+        'border': {
+          DEFAULT: '#DCDFE6',
+          800: '#E4E7ED',
+          900: '#EBEEF5',
+          950: '#F2F6FC',
+        },
+        bg: {
+          DEFAULT: '#F5F7FA',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
